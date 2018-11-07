c5af42037f8a5100a27aa30cd461b970bbf5f7b3
Martastain <martas@imm.cz>
2018-11-07 15:58:52 +0000

    updated updater

--- update-cert.sh
+++ update-cert.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-for config in config/*.ini; do
-    certbot certonly --agree-tos -n -c $config
+for config in config/*; do
+    domain_name=$(basename $config)
+    echo ""
+    echo "Renewing $domain_name"
+    certbot certonly --agree-tos --expand -n -c $config --cert-name $domain_name
 done
 
+nginx -s reload
